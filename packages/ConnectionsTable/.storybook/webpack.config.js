c3644262bf72c28709f1851be5e7a9a8cf802f0f
AlanSl <alan.slater@nearform.com>
2019-10-17 09:37:18 +0100

    fixup
    files wrong way around

--- packages/ConnectionsTable/.storybook/webpack.config.js
+++ packages/ConnectionsTable/.storybook/webpack.config.js
@@ -1,15 +1,2 @@
-module.exports = async ({ config }) => {
-  config.module.rules.push({
-    test: /\.mock$/,
-    use: 'arraybuffer-loader',
-  })
-
-  config.module.rules.push({
-    test: /\.(png|woff|woff2|eot|ttf)$/,
-    loader: 'file-loader',
-  })
-
-  config.devtool = 'source-map'
-
-  return config
-}
+const { webpackConfig } = require('sdk')
+module.exports = webpackConfig
--- packages/sdk/storybook/webpackConfig.js
+++ packages/sdk/storybook/webpackConfig.js
@@ -1,2 +1,15 @@
-const { webpackConfig } = require('sdk')
-module.exports = webpackConfig
+module.exports = async ({ config }) => {
+  config.module.rules.push({
+    test: /\.mock$/,
+    use: 'arraybuffer-loader',
+  })
+
+  config.module.rules.push({
+    test: /\.(png|woff|woff2|eot|ttf)$/,
+    loader: 'file-loader',
+  })
+
+  config.devtool = 'source-map'
+
+  return config
+}
