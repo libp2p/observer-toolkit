51171503988bd62a6a5bebb041553ef773b56960
AlanSl <alan.slater@nearform.com>
2019-10-24 08:56:29 +0100

    Move connection column definitions to definitions dir

--- packages/connections-table/definitions/connectionsColumns.js
+++ packages/connections-table/definitions/connectionsColumns.js
@@ -1,61 +1,14 @@
-import React from 'react'
-import T from 'prop-types'
-
 import { getAge, getTraffic, statusNames, transportNames } from 'proto'
 
+import { getStringSorter, getNumericSorter } from 'sdk'
+
 import {
-  PeerId,
-  TimeNumber,
-  DataNumber,
-  getStringSorter,
-  getNumericSorter,
-} from 'sdk'
+  AgeContent,
+  BytesContent,
+  PeerIdContent,
+} from '../components/cellContent'
 
 import * as statusSorter from '../utils/statusSorter'
-
-// Content renderers:
-// TODO: move to components dir
-
-function PeerIdContent({ value }) {
-  return (
-    <PeerId onClick={() => copyToClipboard(value)} id={value}>
-      Copy "{value}" to the clipboard
-    </PeerId>
-  )
-}
-PeerIdContent.propTypes = {
-  value: T.string,
-}
-
-function BytesContent({ value, label }) {
-  return (
-    <DataNumber value={value}>
-      {`${value} ${label} bytes during this connection's lifecycle`}
-    </DataNumber>
-  )
-}
-BytesContent.propTypes = {
-  value: T.num,
-  label: T.string,
-}
-
-function AgeContent({ value }) {
-  return (
-    <TimeNumber value={value}>
-      {`Connection was open for ${value} miliseconds`}
-    </TimeNumber>
-  )
-}
-AgeContent.propTypes = {
-  value: T.string,
-}
-
-function copyToClipboard(text) {
-  // TODO: expand this and include a toast notice on success
-  navigator.clipboard.writeText(text)
-}
-
-// Column definitions:
 
 const stringSorter = {
   getSorter: getStringSorter,
