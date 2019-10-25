b57018ba5a0c613bbc8b6131c2cbf8abc255bf47
AlanSl <alan.slater@nearform.com>
2019-10-25 02:05:29 +0100

    Use craco to make CreateReactApp transpile lerna siblings

---
+++ packages/catalogue/craco.config.js
@@ -0,0 +1,20 @@
+// crago.config.js
+// see: https://github.com/sharegate/craco
+
+const path = require('path')
+const fs = require('fs')
+const cracoBabelLoader = require('craco-babel-loader')
+
+const appDirectory = fs.realpathSync(process.cwd())
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
+
+module.exports = {
+  plugins: [
+    {
+      plugin: cracoBabelLoader,
+      options: {
+        includes: [resolveApp('../sdk'), resolveApp('../connections-table')],
+      },
+    },
+  ],
+}
