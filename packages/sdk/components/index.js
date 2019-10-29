c0fbf29804e1d7181b96767673211d7e16add77e
AlanSl <alan.slater@nearform.com>
2019-10-29 10:48:17 +0000

    Fix duplicated line after merge from master

--- packages/sdk/components/index.js
+++ packages/sdk/components/index.js
@@ -2,7 +2,6 @@ export * from './context'
 export * from './input'
 export * from './DataTable'
 export * from './Timeline'
-export * from './input'
 
 export { default as PeerId } from './PeerId'
 export { default as Icon } from './Icon'
