8032d8f6f5dc2442ecf3a3798377b68bcf7ab60f
Glen Keane <glenkeane.94@gmail.com>
2019-11-19 21:05:51 +0000

    add basic checksum support

--- packages/proto/utils/binary.js
+++ packages/proto/utils/binary.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const { State } = require('../protobuf/introspection_pb')
+const fnv1a = require('./fnv1a')
 
 function parseBuffer(buf) {
   // Expects a binary file with this repeating structure:
   // - 4-byte version number
-  // - 4-byte length of following State message
-  // - State message
+  // - 4-byte checksum of following message
+  // - 4-byte length of following message
+  // - message (typically state)
 
   const byteLength = Buffer.byteLength(buf)
 
@@ -14,13 +16,17 @@ function parseBuffer(buf) {
   const messages = []
 
   const versionNumberLength = 4
+  const stateMessageChecksumLength = 4
   const stateMessageSizeLength = 4
+
+
+  // Skip version number
+  bytesParsed += versionNumberLength
 
   // TODO - add async variant
   while (bytesParsed < byteLength) {
-    // Skip version number
-    bytesParsed += versionNumberLength
-
+    const stateMessageChecksum = buf.readUIntLE(bytesParsed, checksumLength)
+    bytesParsed += stateMessageChecksumLength
     const stateMessageSize = buf.readUIntLE(bytesParsed, stateMessageSizeLength)
     const stateMessageStart = bytesParsed + stateMessageSizeLength
     const stateMessageEnd = stateMessageStart + stateMessageSize
@@ -30,26 +36,32 @@ function parseBuffer(buf) {
       stateMessageEnd
     )
 
-    const stateMessage = State.deserializeBinary(stateMessageBin)
+    const validChecksum = stateMessageChecksum === fnv1a(stateMessageBin)
 
-    messages.push(stateMessage)
+    // TODO: bubble an error message for an invalid checksum
+    if (validChecksum) {
+      const stateMessage = State.deserializeBinary(stateMessageBin)
+      messages.push(stateMessage)
+    }
     bytesParsed = stateMessageEnd
   }
 
   return messages
 }
 
-function createBufferSegment(state, version) {
+function createBufferSegment(state) {
   const { buffer, byteLength } = state.serializeBinary()
+  const checksum = fnv1a(buffer)
 
   // Mimics the output format of go-libp2p-introspection's ws writer.
-  // First 4 bytes is an unused version number that may be skipped for now.
+  // First is a 4-byte LE integer that is a checksum for the following buffer
   // Then a 4-byte LE integer stating the byte length of a state / timebar position.
-  const lengthBuffer = Buffer.alloc(8)
-  lengthBuffer.writeUIntLE(byteLength, 4, 4)
+  const metaBuffer = Buffer.alloc(8)
+  metaBuffer.writeUIntLE(checksum, 0, 4)
+  metaBuffer.writeUIntLE(byteLength, 4, 4)
 
   const contentBuffer = Buffer.from(buffer)
-  return Buffer.concat([lengthBuffer, contentBuffer])
+  return Buffer.concat([metaBuffer, contentBuffer])
 }
 
 module.exports = {
