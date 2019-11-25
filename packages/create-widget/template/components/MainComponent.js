887ae880476e32d78502b1701b19dd4630a9af74
AlanSl <alan.slater@nearform.com>
2019-11-25 10:34:11 +0000

    Make new widget example content useful

--- packages/create-widget/template/components/MainComponent.js
+++ packages/create-widget/template/components/MainComponent.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import T from 'prop-types'
 import styled from 'styled-components'
 
-const StyledExample = styled.div`
-  background: ${({ theme }) => theme.color('light', 'mid')};
-  padding: ${({ theme }) => theme.spacing(2)};
-  border-radius: ${({ theme }) => theme.spacing()};
+import { getTime, getTimeIndex } from '@libp2p-observer/data'
+import { DataContext, TimeContext } from '@libp2p-observer/sdk'
+
+const ExampleStyledHeader = styled.h2`
   ${({ theme }) => theme.text('body', 'large')}
+`
+
+const ExampleStyledContent = styled.div`
+  background: ${({ theme }) => theme.color('light', 'mid')};
+  border-radius: ${({ theme }) => theme.spacing()};
+  padding: ${({ theme }) => theme.spacing(2)};
 `
 
 function $WIDGET_COMPONENT({ children }) {
   // Function component logic here
   // Use the React Hooks model, see https://reactjs.org/docs/hooks-intro.html
+  const timepoints = useContext(DataContext)
+  const currentTimepoint = useContext(TimeContext)
+
+  const time = getTime(currentTimepoint)
+  const timeIndex = getTimeIndex(timepoints, time)
+
+  /* eslint-disable-next-line no-console */
+  console.log(time, currentTimepoint, timepoints)
 
   return (
-    <StyledExample>
-      Your component content here
+    <ExampleStyledContent>
+      <ExampleStyledHeader>Hello $WIDGET_COMPONENT</ExampleStyledHeader>
+      <p>
+        Currently selected time point {timeIndex + 1} of {timepoints.length}
+        reffering to {new Date(time).toLocaleString()}. Open console to inspect.
+      </p>
       {children}
-    </StyledExample>
+    </ExampleStyledContent>
   )
 }
 
