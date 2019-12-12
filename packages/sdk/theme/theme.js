ebb53e8278fdffaf4ed4ff22913cfd517e541a16
AlanSl <alan.slater@nearform.com>
2019-12-12 17:15:05 +0000

    Adjust theme typography styles

--- packages/sdk/theme/theme.js
+++ packages/sdk/theme/theme.js
@@ -66,42 +66,49 @@ const typography = {
   body: {
     small: `
       font-size: 9pt;
+      line-height: 1.6em;
     `,
     medium: `
-      font-size: 12pt;
+      font-size: 10pt;
+      line-height: 1.6em;
     `,
     large: `
-      font-size: 16pt;
+      font-size: 12pt;
+      line-height: 1.6em;
     `,
   },
   label: {
     small: `
       font-size: 8pt;
-      line-height: 1em;
+      line-height: 1.2em;
     `,
     medium: `
-      font-size: 10pt;
-      line-height: 1em;
+      font-size: 9pt;
+      line-height: 1.2em;
     `,
     large: `
-      font-size: 12pt;
-      line-height: 1em;
+      font-size: 10pt;
+      line-height: 1.2em;
     `,
   },
   heading: {
     small: `
-      font-size: 12pt;
-      margin: ${spacing()} 0;
-      font-weight: bold;
+      font-size: 10pt;
+      margin: 0;
+      font-weight: 800;
+      line-height: 1.2em;
     `,
     medium: `
-      font-size: 16pt;
-      margin: ${spacing(2)} 0 ${spacing()};
+      font-size: 12pt;
+      margin: ${spacing([0.5, 0])};
+      font-weight: 800;
+      line-height: 1.2em;
     `,
     large: `
-      font-size: 24pt;
-      margin: ${spacing(2)} 0 ${spacing()};
-      font-weight: bold;
+      font-size: 16pt;
+      margin: ${spacing([0.5, 0])};
+      font-weight: 600;
+      line-height: 1.2em;
     `,
   },
 }
