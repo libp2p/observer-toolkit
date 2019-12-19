7bea4dcc7f60b40e9f8514d181f92a4c189e3a9d
AlanSl <alan.slater@nearform.com>
2019-12-19 00:08:28 +0000

    Add useCalculation hook for calculatableProp in range silder

--- packages/connections-table/definitions/filterDefs.js
+++ packages/connections-table/definitions/filterDefs.js
@@ -1,7 +1,23 @@
-import { getListFilter, getRangeFilter } from '@libp2p-observer/sdk'
-import { getStreams, statusNames } from '@libp2p-observer/data'
+import {
+  getListFilter,
+  getRangeFilter,
+  useCalculation,
+} from '@libp2p-observer/sdk'
+import { getConnections, getStreams, statusNames } from '@libp2p-observer/data'
 
 const statusNamesList = Object.values(statusNames)
+
+function getMaxStreams() {
+  const maxStreams = useCalculation(
+    ({ timepoint }) =>
+      getConnections(timepoint).reduce(
+        (max, conn) => Math.max(max, getStreams(conn).length),
+        0
+      ),
+    ['timepoint']
+  )
+  return maxStreams
+}
 
 const filterDefs = [
   getListFilter({
@@ -13,6 +29,7 @@ const filterDefs = [
     name: 'Filter number of streams',
     mapFilter: conn => getStreams(conn).length,
     min: 0,
+    max: getMaxStreams,
   }),
 ]
 
--- packages/sdk/utils/helpers.js
+++ packages/sdk/utils/helpers.js
@@ -11,18 +11,42 @@ function copyToClipboard(text) {
   navigator.clipboard.writeText(text)
 }
 
+function calculatableProp(prop, allowUndefined = false) {
+  // Handle and validate props that may be numbers or functions returning a number
+  if (allowUndefined && prop === undefined) return prop
+  const num = typeof prop === 'function' ? prop() : prop
+  throwIf(isNotNumeric(num, 'calculatableProp'))
+  return num
+}
+
+function isNotNumeric(value, name = 'Value') {
+  // Return throwable error message if NaN or non-number
+  return isNaN(value)
+    ? `${name} is not numeric, is ${typeof value} (${value})`
+    : false
+}
+
+function throwIf(errorMsg) {
+  if (errorMsg && typeof errorMsg === 'string') throw new Error(errorMsg)
+  if (Array.isArray(errorMsg) && errorMsg.length)
+    throw new Error(errorMsg.join('\n\n '))
+}
+
 // d3 data processors can fail very late if NaN or similar creep in.
 // Use this to fail early in memoised or non-perf-intensive functions.
 function validateNumbers(obj) {
-  const errors = Object.entries(obj).reduce(
-    (errors, [key, num]) =>
-      isNaN(num)
-        ? [...errors, `${key} is not numeric, is ${typeof num}: ${num}`]
-        : errors,
-    []
-  )
-
-  if (errors.length) throw new Error(errors.join('\n\n '))
+  const errors = Object.entries(obj).reduce((errors, [key, num]) => {
+    const errorMsg = isNotNumeric(num, key)
+    return errorMsg ? [...errors, errorMsg] : errors
+  }, [])
+  throwIf(errors)
 }
 
-export { childrenToString, copyToClipboard, validateNumbers }
+export {
+  calculatableProp,
+  childrenToString,
+  copyToClipboard,
+  isNotNumeric,
+  throwIf,
+  validateNumbers,
+}
