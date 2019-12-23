4d58cf41b7e2a52f16e40df085efe24d4fc7d0da
AlanSl <alan.slater@nearform.com>
2019-12-23 17:22:02 +0000

    Update name of util function in test

--- packages/catalogue/src/definitions/utils.test.js
+++ packages/catalogue/src/definitions/utils.test.js
@@ -1,6 +1,6 @@
 import * as utils from './utils'
 
-describe('validateComponentFields only allows valid component fields in supplied object', () => {
+describe('validateWidgetFields only allows valid component fields in supplied object', () => {
   const valid = {
     name: 'validName',
     Component: () => {},
@@ -9,11 +9,11 @@ describe('validateComponentFields only allows valid component fields in supplied
   }
 
   it('passes with all valid fields', () => {
-    expect(() => utils.validateComponentFields(valid)).not.toThrow()
+    expect(() => utils.validateWidgetFields(valid)).not.toThrow()
   })
 
   it('throws if component bundle is undefined', () => {
-    expect(() => utils.validateComponentFields()).toThrow(
+    expect(() => utils.validateWidgetFields()).toThrow(
       new Error(
         'Invalid component type, must be an object with properties `name`, `Component`, `description`, `tags` and optionally `screenshot`'
       )
@@ -22,29 +22,29 @@ describe('validateComponentFields only allows valid component fields in supplied
 
   it('throws if name is not supplied', () => {
     expect(() =>
-      utils.validateComponentFields({ ...valid, name: undefined })
+      utils.validateWidgetFields({ ...valid, name: undefined })
     ).toThrow(new Error('Invalid component name "undefined" (undefined)'))
   })
   it('throws if name is not a string', () => {
-    expect(() =>
-      utils.validateComponentFields({ ...valid, name: 123 })
-    ).toThrow(new Error('Invalid component name "123" (number)'))
+    expect(() => utils.validateWidgetFields({ ...valid, name: 123 })).toThrow(
+      new Error('Invalid component name "123" (number)')
+    )
   })
 
   it('throws if Component is not supplied', () => {
     expect(() =>
-      utils.validateComponentFields({ ...valid, Component: undefined })
+      utils.validateWidgetFields({ ...valid, Component: undefined })
     ).toThrow(new Error('Invalid Component renderer for validName (undefined)'))
   })
   it('throws if Component is not a function', () => {
     expect(() =>
-      utils.validateComponentFields({ ...valid, Component: 'invalid' })
+      utils.validateWidgetFields({ ...valid, Component: 'invalid' })
     ).toThrow(new Error('Invalid Component renderer for validName (string)'))
   })
 
   it('throws if description is not supplied', () => {
     expect(() =>
-      utils.validateComponentFields({ ...valid, description: undefined })
+      utils.validateWidgetFields({ ...valid, description: undefined })
     ).toThrow(
       new Error(
         'Invalid component description "undefined" for validName (undefined)'
@@ -53,7 +53,7 @@ describe('validateComponentFields only allows valid component fields in supplied
   })
   it('throws if description is not a string', () => {
     expect(() =>
-      utils.validateComponentFields({ ...valid, description: 123 })
+      utils.validateWidgetFields({ ...valid, description: 123 })
     ).toThrow(
       new Error('Invalid component description "123" for validName (number)')
     )
@@ -61,12 +61,12 @@ describe('validateComponentFields only allows valid component fields in supplied
 
   it('throws if tags is not supplied', () => {
     expect(() =>
-      utils.validateComponentFields({ ...valid, tags: undefined })
+      utils.validateWidgetFields({ ...valid, tags: undefined })
     ).toThrow(new Error('Invalid component tags for validName (undefined)'))
   })
   it('throws if tags is not an array', () => {
-    expect(() =>
-      utils.validateComponentFields({ ...valid, tags: 123 })
-    ).toThrow(new Error('Invalid component tags for validName (number)'))
+    expect(() => utils.validateWidgetFields({ ...valid, tags: 123 })).toThrow(
+      new Error('Invalid component tags for validName (number)')
+    )
   })
 })
