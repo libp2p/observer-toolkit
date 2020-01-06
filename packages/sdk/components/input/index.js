bb20ee6d20a555ec26de07f708e00aaf04334069
AlanSl <alan.slater@nearform.com>
2020-01-06 16:38:59 +0000

    Fix issue with formik context lost in Jest

--- packages/sdk/components/input/index.js
+++ packages/sdk/components/input/index.js
@@ -1,3 +1,8 @@
+// Jest has issues resolving peer deps correctly, which can duplicate
+// Formik if another package imports it directly. Export it so anything
+// importing from the SDK 100% definitely gets the same contexts.
+export { Formik } from 'formik'
+
 export * from './fields'
 
 export { default as StyledButton } from './StyledButton'
