c1d447ea1777c805c86f54d91d51199e243cad8f
Osmond van Hemert <osmond@vanhemert.nu>
2020-01-24 11:28:09 +0100

    fix lint error

--- packages/sdk/components/input/WebSocketDataButton.js
+++ packages/sdk/components/input/WebSocketDataButton.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import T from 'prop-types'
 import styled from 'styled-components'
 
