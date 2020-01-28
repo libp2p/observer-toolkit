a04fddf861ff80eae2d1438a3a7e42054552bf0f
AlanSl <alan.slater@nearform.com>
2020-01-28 17:08:38 +0000

    Expand App tests, fixing fetch() to work in any package's tests

--- __jest__/setupTest.js
+++ __jest__/setupTest.js
@@ -4,5 +4,32 @@ import 'jest-styled-components'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import '@testing-library/jest-dom/extend-expect'
+import fs from 'fs'
+import path from 'path'
 
 jest.setTimeout(10000)
+
+global.fetch = async filepath => {
+  if (path.extname(filepath) === '.mock') {
+    const samplesPath = path.dirname(
+      require.resolve('@libp2p-observer/samples')
+    )
+    filepath = path.resolve(samplesPath, 'samples', path.basename(filepath))
+  }
+
+  const buffer = await fs.promises.readFile(filepath)
+  return {
+    ok: !!buffer,
+    arrayBuffer: async () => {
+      const arrayBuffer = new ArrayBuffer(buffer.length)
+      const view = new Uint8Array(arrayBuffer)
+      let i = 0
+      const length = buffer.length
+      while (i < length) {
+        view[i] = buffer[i]
+        i++
+      }
+      return buffer
+    },
+  }
+}
--- packages/shell/components/DataTray/SamplesList.test.js
+++ packages/shell/components/DataTray/SamplesList.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { fireEvent, act } from '@testing-library/react'
 import waitForExpect from 'wait-for-expect'
-import fs from 'fs'
 
 import {
   renderWithData,
@@ -9,29 +8,8 @@ import {
   loadSample,
   within,
 } from '@libp2p-observer/testing'
-import samples from '@libp2p-observer/samples'
 
 import SamplesList from './SamplesList'
-
-beforeEach(() => {
-  window.fetch = async path => {
-    const buffer = await fs.promises.readFile(path)
-    return {
-      ok: true,
-      arrayBuffer: async () => {
-        const arrayBuffer = new ArrayBuffer(buffer.length)
-        const view = new Uint8Array(arrayBuffer)
-        let i = 0
-        const length = buffer.length
-        while (i < length) {
-          view[i] = buffer[i]
-          i++
-        }
-        return buffer
-      },
-    }
-  }
-})
 
 describe('SamplesList', () => {
   it('renders as expected', () => {
