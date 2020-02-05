d68df02ff54c6ed36ae91b400d41415e9e5c389c
AlanSl <alan.slater@nearform.com>
2020-02-05 07:00:11 +0000

    Run create-widget script

---
+++ packages/dht-buckets/jsx-packages.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// These packages in our repo export JSX and we must tell webpack to use babel loader on them
+const jsxPackages = [
+  'catalogue',
+  'connections-table',
+  'sdk',
+  'shell',
+  'streams-table',
+  'testing',
+]
+
+// Regex for all other packages
+const nonJsxPackagesRegex = new RegExp(
+  `node_modules[\\\\/](?!@libp2p-observer[\\\\/](${jsxPackages.join(
+    '|'
+  )}))[\\\\/]`
+)
+
+module.exports = {
+  jsxPackages,
+  nonJsxPackagesRegex,
+}
