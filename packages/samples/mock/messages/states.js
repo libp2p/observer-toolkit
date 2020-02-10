877666a592e0c8f7981fdd8deff349183fae9f51
Osmond van Hemert <osmond@vanhemert.nu>
2020-02-10 16:39:52 +0100

    fix passing dht

--- packages/samples/mock/messages/states.js
+++ packages/samples/mock/messages/states.js
@@ -8,7 +8,7 @@ const { Timestamp } = require('google-protobuf/google/protobuf/timestamp_pb')
 const { SNAPSHOT_DURATION } = require('../utils')
 const { createTraffic, sumTraffic } = require('../messages/traffic')
 
-function createState(connectionsList, now) {
+function createState(connectionsList, now, dht) {
   const state = new State()
 
   state.setInstantTs(new Timestamp([now]))
@@ -21,7 +21,7 @@ function createState(connectionsList, now) {
 
   const subsystems = new Subsystems()
   subsystems.setConnectionsList(connectionsList)
-  // subsystems.setDht(dht)
+  subsystems.setDht(dht)
 
   state.setSubsystems(subsystems)
 
