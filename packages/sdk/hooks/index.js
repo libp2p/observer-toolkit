311e059b3ab71c8b087d3441ea1608f62b20a051
AlanSl <alan.slater@nearform.com>
2020-02-14 17:14:38 +0000

    Export hook

--- packages/sdk/hooks/index.js
+++ packages/sdk/hooks/index.js
@@ -4,6 +4,7 @@ export { default as useFilter } from './useFilter'
 export { default as useSorter } from './useSorter'
 
 // Data formatters
+export { default as usePooledData } from './usePooledData'
 export { default as useStackedData } from './useStackedData'
 export { default as useTabularData } from './useTabularData'
 
