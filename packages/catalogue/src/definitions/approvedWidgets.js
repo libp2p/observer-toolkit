b1b95c60f4d5f9ea0da47deb21ca8ce9a03e9c9c
AlanSl <alan.slater@nearform.com>
2020-02-24 05:44:36 +0000

    Fix dht-buckets config to show in catalogue

--- packages/catalogue/src/definitions/approvedWidgets.js
+++ packages/catalogue/src/definitions/approvedWidgets.js
@@ -2,10 +2,12 @@ import { validateWidgetFields } from './utils'
 
 import * as connectionsTable from '@libp2p-observer/connections-table'
 import * as streamsTable from '@libp2p-observer/streams-table'
+import * as dhtBuckets from '@libp2p-observer/dht-buckets'
 
 const approvedWidgets = [
   validateWidgetFields(connectionsTable),
   validateWidgetFields(streamsTable),
+  validateWidgetFields(dhtBuckets),
 ]
 
 export default approvedWidgets
---
+++ packages/dht-buckets/Widget.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import T from 'prop-types'
+
+import { ActiveWidget } from '@libp2p-observer/shell'
+import { name, description } from './metadata'
+import WidgetContext from './components/context/WidgetContext'
+import DhtBuckets from './components/DhtBuckets'
+
+function Widget({ closeWidget }) {
+  return (
+    <WidgetContext>
+      <ActiveWidget
+        name={name}
+        description={description}
+        closeWidget={closeWidget}
+      >
+        <DhtBuckets />
+      </ActiveWidget>
+    </WidgetContext>
+  )
+}
+
+Widget.propTypes = {
+  closeWidget: T.func,
+}
+
+export default Widget
