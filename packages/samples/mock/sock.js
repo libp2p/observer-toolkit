ad52fba39001c00124af3937b686eecdaec90d93
Alan Slater <alan.slater@nearform.com>
2020-03-02 13:01:20 +0000

    Add and send events messages to websockets (#45)
    * update protobuf
    
    * initial events commit
    
    * send regular empty event
    
    * fix linting
    
    * send event when connection opens
    
    * add randomness to event ts
    
    * cleanup comments
    
    * sync timestamps
    
    * Handle states arrays with only one item
    
    The timeline's data stacker uses the difference between each pair of timepoints. Previously we'd never had to handle a states array of length === 1, which would cause useStackedData to deal with an unexpected 0-length array despite having checked that data was present.
    
    * cleanup code
    
    * cleanup code
    
    * Update for latest DHT mocking reqs
    
    Co-authored-by: Osmond van Hemert <hello@ovhemert.dev>

---
+++ packages/samples/mock/sock.js
@@ -0,0 +1,128 @@
+'use strict'
+
+const http = require('http')
+const WebSocket = require('ws')
+const {
+  proto: { ClientSignal },
+} = require('@libp2p-observer/proto')
+const { random } = require('./utils')
+const {
+  generateConnections,
+  generateDHT,
+  generateEvent,
+  generateRuntime,
+  generateState,
+  generateVersion,
+  updateConnections,
+  updateDHT,
+} = require('./generate')
+
+const connections = []
+const dht = generateDHT()
+const version = generateVersion()
+const runtime = generateRuntime()
+const server = http.createServer()
+const wss = new WebSocket.Server({ noServer: true })
+// let tmrMessages
+// let tmrQueueProcess
+
+const msgQueue = []
+
+function generateMessages({ connectionsCount }) {
+  const utcNow = Date.now()
+
+  if (!connections.length) {
+    connections.length = 0
+    const conns = generateConnections(connectionsCount, utcNow - 1000)
+    updateConnections(conns, null, utcNow)
+    connections.push(...conns)
+  } else {
+    updateConnections(connections, connectionsCount, utcNow)
+  }
+
+  // send event when connection is opening/closing
+  connections
+    .filter(cn => cn.getStatus() === 2 || cn.getStatus() === 3)
+    .forEach(cn => {
+      const now = utcNow + Math.floor(random() * 800 + 100)
+      const type = cn.getStatus() === 2 ? 'opening' : 'closing'
+      const content = { peerId: cn.getPeerId() }
+      const event = generateEvent({ now, type, content })
+      const data = Buffer.concat([version, runtime, event]).toString('binary')
+      msgQueue.push({ ts: now, type: 'event', data })
+    })
+
+  updateDHT(dht, connections, utcNow, utcNow + 1000)
+
+  const state = generateState(connections, utcNow, dht)
+  const data = Buffer.concat([version, runtime, state]).toString('binary')
+  msgQueue.push({ ts: utcNow + 1000, type: 'state', data })
+}
+
+function sendQueue(ws) {
+  const utcNow = Date.now()
+  const queue = []
+  msgQueue.forEach((item, idx) => {
+    if (item.ts <= utcNow) {
+      queue.push(msgQueue.splice(idx, 1)[0])
+    }
+  })
+  queue
+    .sort((a, b) => a.ts - b.ts)
+    .forEach(item => {
+      ws.send(item.data)
+    })
+}
+
+function handleClientMessage(ws, msg) {
+  // check client signal
+  if (msg) {
+    const clientSignal = ClientSignal.deserializeBinary(msg)
+    const signal = clientSignal.getSignal()
+    if (signal === ClientSignal.Signal.SEND_DATA) {
+      sendQueue(ws)
+    } else if (
+      signal === ClientSignal.Signal.START_PUSH_EMITTER ||
+      signal === ClientSignal.Signal.UNPAUSE_PUSH_EMITTER
+    ) {
+      // TODO: implement unpause/start diff of timer emitter
+      setInterval(() => {
+        sendQueue(ws)
+      }, 200)
+    } else if (
+      signal === ClientSignal.Signal.STOP_PUSH_EMITTER ||
+      signal === ClientSignal.Signal.PAUSE_PUSH_EMITTER
+    ) {
+      // TODO: implement pause/stop of timer emitter
+    }
+  }
+}
+
+function start({ connectionsCount = 0 }) {
+  // generate states
+  setInterval(() => {
+    generateMessages({ connectionsCount })
+  }, 1000)
+
+  // handle messages
+  wss.on('connection', ws => {
+    ws.on('message', msg => {
+      handleClientMessage(ws, msg)
+    })
+  })
+
+  server.on('upgrade', function upgrade(request, socket, head) {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request)
+    })
+  })
+
+  // listen for connections
+  server.listen(8080, () => {
+    console.log('Websocket server listening on ws://localhost:8080')
+  })
+}
+
+module.exports = {
+  start,
+}
