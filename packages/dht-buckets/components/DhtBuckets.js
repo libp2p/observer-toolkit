f473cf67c2700f53d0105f7d8ccadec5931f06c6
AlanSl <alan.slater@nearform.com>
2020-03-04 09:29:10 +0000

    Remove obsolete files

--- packages/dht-buckets/components/DhtBuckets.js
+++ packages/dht-buckets/components/DhtBuckets.js
@@ -1,9 +1,20 @@
 import React, { useContext } from 'react'
 import T from 'prop-types'
+import styled from 'styled-components'
 
 import { getAllDhtBuckets, getTime } from '@libp2p-observer/data'
 import { TimeContext } from '@libp2p-observer/sdk'
-import DhtActivePeers from './DhtActivePeers'
+
+import DhtColumn from './DhtColumn/DhtColumn'
+
+const Container = styled.div`
+  background: ${({ theme, backgroundColorIndex }) =>
+    theme.color('background', 1)};
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  padding: ${({ theme }) => theme.spacing([1, 0.5])};
+`
 
 function DhtBuckets({ children }) {
   const currentState = useContext(TimeContext)
@@ -13,11 +24,23 @@ function DhtBuckets({ children }) {
   const { [0]: catchAllBucketPeers, ...numberedBucketPeers } = buckets
 
   return (
-    <DhtActivePeers
-      catchAllBucketPeers={catchAllBucketPeers}
-      numberedBucketPeers={numberedBucketPeers}
-      timestamp={timestamp}
-    />
+    <Container>
+      <DhtColumn
+        peers={catchAllBucketPeers}
+        timestamp={timestamp}
+        bucketNum={0}
+        key={`bucket_0`}
+        title="0 — Catch-all"
+      />
+      {Object.entries(numberedBucketPeers).map(([bucketStr, peers]) => (
+        <DhtColumn
+          peers={peers}
+          timestamp={timestamp}
+          bucketNum={Number(bucketStr)}
+          key={`bucket_${bucketStr}`}
+        />
+      ))}
+    </Container>
   )
 }
 
