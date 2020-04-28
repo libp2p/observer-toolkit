ee131a5b4586525ac6b6ecb18af5df50ee6d6c72
AlanSl <alan.slater@nearform.com>
2020-04-28 11:22:02 +0100

    Add tooltip to pause/unpause shell control

--- packages/shell/components/DataPanel/WebsocketControl.js
+++ packages/shell/components/DataPanel/WebsocketControl.js
@@ -1,7 +1,12 @@
 import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 
-import { Icon, SetterContext, WebsocketContext } from '@libp2p-observer/sdk'
+import {
+  Icon,
+  SetterContext,
+  Tooltip,
+  WebsocketContext,
+} from '@libp2p-observer/sdk'
 
 // TODO: animate circle for incoming state using stroke-dasharray transition
 // like in https://stackoverflow.com/questions/26178095/svg-circle-animation
@@ -20,6 +25,12 @@ const IconButton = styled.button`
   z-index: 2;
 `
 
+const TooltipContent = styled.div`
+  color: ${({ theme }) => theme.color('highlight')};
+  font-weight: 600;
+  white-space: nowrap;
+`
+
 function WebsocketControl() {
   const [hasFocus, setHasFocus] = useState(false)
   const { isPaused, sendSignal } = useContext(WebsocketContext) || {}
@@ -27,6 +38,8 @@ function WebsocketControl() {
 
   const showPauseIcon = hasFocus ? !isPaused : isPaused
   const iconType = showPauseIcon ? 'pause' : 'play'
+
+  const tooltipText = isPaused ? 'Unpause incoming data' : 'Pause incoming data'
 
   const handleFocus = e => {
     e.stopPropagation()
@@ -52,17 +65,24 @@ function WebsocketControl() {
 
   // TODO: Add tooltip on the right saying "Pause" / "Unpause"
   return (
-    <IconButton
-      tabIndex={0}
-      onClick={handleClick}
-      onMouseEnter={handleFocus}
-      onFocus={handleFocus}
-      onMouseLeave={handleBlur}
-      onBlur={handleBlur}
-      hasFocus={hasFocus}
+    <Tooltip
+      fixOn="never"
+      side="right"
+      toleranceY={null}
+      content={<TooltipContent>{tooltipText}</TooltipContent>}
     >
-      <Icon type={iconType} />
-    </IconButton>
+      <IconButton
+        tabIndex={0}
+        onClick={handleClick}
+        onMouseEnter={handleFocus}
+        onFocus={handleFocus}
+        onMouseLeave={handleBlur}
+        onBlur={handleBlur}
+        hasFocus={hasFocus}
+      >
+        <Icon type={iconType} />
+      </IconButton>
+    </Tooltip>
   )
 }
 
