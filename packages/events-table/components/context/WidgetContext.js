9e27840ad0abf951ae7f2c0e73cf3291a4c9aeee
AlanSl <alan.slater@nearform.com>
2020-05-01 09:55:32 +0100

    Update events widget filters

--- packages/events-table/components/context/WidgetContext.js
+++ packages/events-table/components/context/WidgetContext.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import T from 'prop-types'
 
-import { getListFilter, FilterProvider } from '@libp2p-observer/sdk'
-import { statusNames } from '@libp2p-observer/data'
-
-const statusNamesList = Object.values(statusNames)
+import {
+  getListFilter,
+  getRangeFilter,
+  DataContext,
+  FilterProvider,
+  RuntimeContext,
+  TimeContext,
+} from '@libp2p-observer/sdk'
+import { getEventType } from '@libp2p-observer/data'
 
 function WidgetContext({ children }) {
+  const runtime = useContext(RuntimeContext)
+  const states = useContext(DataContext)
+  const currentTimepoint = useContext(TimeContext)
+
+  const firstStateTime = (states[0] && states[0].getInstantTs()) || 0
+  const lastStateTime = currentTimepoint ? currentTimepoint.getInstantTs() : 0
+
   const filterDefs = [
+    getRangeFilter({
+      name: 'Filter by time',
+      mapFilter: msg => (msg.getTs ? msg.getTs() : 0),
+      min: firstStateTime,
+      max: lastStateTime,
+      stepInterval: 1,
+      numberFieldType: 'time',
+    }),
     getListFilter({
-      name: 'Filter by status',
-      mapFilter: ({ stream }) => statusNamesList[stream.getStatus()],
-      valueNames: statusNamesList,
+      name: 'Filter event types',
+      mapFilter: msg => {
+        if (!msg.getType) return null
+        const eventType = getEventType(msg)
+        return eventType || null
+      },
+      valueNames: runtime
+        ? runtime.getEventTypesList().map(type => type.getName())
+        : [],
     }),
   ]
 
