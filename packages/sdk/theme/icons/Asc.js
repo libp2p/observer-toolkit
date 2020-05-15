22747e44340276a4407eab75898c1e03506fdf72
AlanSl <alan.slater@nearform.com>
2020-05-15 10:20:28 +0100

    Update icon JS styling for consistency

--- packages/sdk/theme/icons/Asc.js
+++ packages/sdk/theme/icons/Asc.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import * as React from 'react'
 
-const SvgAsc = props => (
-  <svg
-    viewBox="16 16 70 70"
-    fill="currentColor"
-    strokeLinejoin="round"
-    strokeMiterlimit={1.414}
-    {...props}
-  >
-    <path d="M51.494 35.157l4.41 4.409a1.5 1.5 0 102.121-2.121l-6.965-6.964a1.5 1.5 0 00-2.12 0l-6.957 6.956a1.5 1.5 0 102.121 2.121l4.39-4.389v33.367a1.503 1.503 0 001.5 1.5 1.503 1.503 0 001.5-1.5V35.157z" />
-  </svg>
-)
+function SvgAsc(props) {
+  return (
+    <svg
+      viewBox="0 0 100 125"
+      fill="currentColor"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit={1.414}
+      {...props}
+    >
+      <path d="M51.494 35.157l4.41 4.409a1.5 1.5 0 102.121-2.121l-6.965-6.964a1.5 1.5 0 00-2.12 0l-6.957 6.956a1.5 1.5 0 102.121 2.121l4.39-4.389v33.367a1.503 1.503 0 001.5 1.5 1.503 1.503 0 001.5-1.5V35.157z" />
+      <text
+        y={115}
+        fill="#000"
+        fontSize={5}
+        fontWeight="bold"
+        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        {'Created by Ryo Sato'}
+      </text>
+      <text
+        y={120}
+        fill="#000"
+        fontSize={5}
+        fontWeight="bold"
+        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        {'from the Noun Project'}
+      </text>
+    </svg>
+  )
+}
 
 export default SvgAsc
--- packages/sdk/theme/icons/Uncheck.js
+++ packages/sdk/theme/icons/Uncheck.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import * as React from 'react'
 
-const SvgUncheck = props => (
-  <svg viewBox="0 0 96 96" {...props}>
-    <defs>
-      <style />
-    </defs>
-    <path
-      d="M48 19a29 29 0 11-29 29 29 29 0 0129-29m0-4a33 33 0 1033 33 33 33 0 00-33-33z"
-      fill="currentColor"
-    />
-  </svg>
-)
+function SvgUncheck(props) {
+  return (
+    <svg viewBox="0 0 96 96" {...props}>
+      <defs>
+        <style />
+      </defs>
+      <path
+        d="M48 19a29 29 0 11-29 29 29 29 0 0129-29m0-4a33 33 0 1033 33 33 33 0 00-33-33z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
 
 export default SvgUncheck
