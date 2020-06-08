d8d308e8ce3b431b7480506eeb57e250fd3acc3e
AlanSl <alan.slater@nearform.com>
2020-06-08 11:24:23 +0100

    Include config in file exports

--- packages/sdk/hooks/useFileBlob.js
+++ packages/sdk/hooks/useFileBlob.js
@@ -5,6 +5,7 @@ import {
   DataContext,
   EventsContext,
   RuntimeContext,
+  ConfigContext,
 } from '../components/context'
 
 const version = 1
@@ -33,7 +34,8 @@ function getServerMessage(message, type) {
     serverMessage.setEvent(message)
   } else if (type === 'state') {
     serverMessage.setState(message)
-    // Don't export response or notice messages
+  } else if (type === 'response') {
+    serverMessage.setResponse(message)
   } else {
     throw new Error(`Unrecognised ServerMessage payload type "${type}"`)
   }
@@ -49,20 +51,32 @@ function getVersionBinary() {
   return versionBuf
 }
 
+function getConfigResponse(config) {
+  const response = new proto.CommandResponse()
+  response.setResult(proto.CommandResponse.Result.OK)
+  response.setEffectiveConfig(config)
+  response.setId(0) // Only one response in static file
+  return response
+}
+
 function useFileBlob() {
   const states = useContext(DataContext)
   const events = useContext(EventsContext)
   const runtime = useContext(RuntimeContext)
+  const config = useContext(ConfigContext)
+  const configResponse = getConfigResponse(config)
 
   const statePackets = states.map(state => getServerMessage(state, 'state'))
   const eventPackets = events.map(event => getServerMessage(event, 'event'))
   const runtimePacket = getServerMessage(runtime, 'runtime')
+  const configPacket = getServerMessage(configResponse, 'response')
 
   const blob = new Blob([
     ...getVersionBinary(),
+    getBinary(runtimePacket),
+    getBinary(configPacket),
     ...statePackets.map(getBinary),
     ...eventPackets.map(getBinary),
-    getBinary(runtimePacket),
   ])
 
   return blob
